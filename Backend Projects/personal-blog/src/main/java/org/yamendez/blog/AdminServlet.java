201e9cd151742c4af9388eea02ce7136c939adc5
yamendez <81581626+yamendez@users.noreply.github.com>
2026-02-15 16:03:10 -0400

    view: admin dashboard

--- Backend Projects/personal-blog/src/main/java/org/yamendez/blog/AdminServlet.java
+++ Backend Projects/personal-blog/src/main/java/org/yamendez/blog/AdminServlet.java
@@ -1,4 +1,17 @@
 package org.yamendez.blog;
 
-public class AdminServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/admin")
+public class AdminServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/admin.html").forward(req, resp);
+    }
 }
