4d370fcfc424e8e23255946307a66696518a461d
yamendez <81581626+yamendez@users.noreply.github.com>
2026-02-15 15:59:54 -0400

    view: article details

--- Backend Projects/personal-blog/src/main/java/org/yamendez/blog/ArticleServlet.java
+++ Backend Projects/personal-blog/src/main/java/org/yamendez/blog/ArticleServlet.java
@@ -1,4 +1,17 @@
 package org.yamendez.blog;
 
-public class ArticleServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/article/1")
+public class ArticleServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/article.html").forward(req, resp);
+    }
 }
